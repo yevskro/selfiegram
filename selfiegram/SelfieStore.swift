826f568a33101577e9a24b917917bcf7b0943232
Yevgeniy Skroznikov <yevgeniyskroznikov@Yevgeniys-MacBook-Air.local>
2018-07-03 11:18:02 -0400

    stubbed out SelfieStore class

--- selfiegram/SelfieStore.swift
+++ selfiegram/SelfieStore.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit.UIImage
 
+// Selfie class to hold the image data
+
 class Selfie : Codable {
     let created : Date
     let id : UUID
@@ -23,4 +25,44 @@ class Selfie : Codable {
             
         }
     }*/
+}
+
+enum SelfieStoreError : Error {
+    case cannotSaveImage(UIImage?)
+}
+
+final class SelfieStore{
+    static let shared = SelfieStore()
+    
+    func getImage(id: UUID) -> UIImage? {
+        // id - unique id of the image
+        // returns the found image if there is one
+        // will be cached into memory for future use
+        return nil
+    }
+    
+    func setImage(id: UUID, image: UIImage?) throws {
+        // saves image to disk with an associated id
+        throw SelfieStoreError.cannotSaveImage(image)
+    }
+    
+    func listSelfies() throws -> [Selfie] {
+        // returns an array of Selfies
+        return []
+    }
+    
+    func delete(selfie: Selfie) throws {
+        // deletes a selfie from the store
+        throw SelfieStoreError.cannotSaveImage(nil)
+    }
+    
+    func load(id: UUID) throws -> Selfie? {
+        return nil
+    }
+    
+    func save(selfie: Selfie) throws {
+        // attempts to save the selfie to disk
+        // throws an error if not possible
+        throw SelfieStoreError.cannotSaveImage(nil)
+    }
 }
