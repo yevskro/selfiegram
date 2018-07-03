2cf3f38b00889d9cc3393ce8840f8a30af2f763c
Yevgeniy Skroznikov <yevgeniyskroznikov@Yevgeniys-MacBook-Air.local>
2018-07-03 11:04:55 -0400

    added selfie class

---
+++ selfiegram/SelfieStore.swift
@@ -0,0 +1,26 @@
+//
+//  SelfiStore.swift
+//  selfiegram
+//
+//  Created by Yevgeniy Skroznikov on 7/3/18.
+//  Copyright © 2018 Yevgeniy Skroznikov. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+
+class Selfie : Codable {
+    let created : Date
+    let id : UUID
+    var title = "New Selfie!"
+    
+    /*
+    var image : UIImage?{
+        get{
+            //return SelfieStore
+        }
+        set{
+            
+        }
+    }*/
+}
